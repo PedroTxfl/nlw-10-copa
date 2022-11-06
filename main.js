7f0a47bc3dcab151121478a385fffbc33785020c
PedroTxfl <pedro.tx2001@gmail.com>
2022-11-06 20:57:56 -0300

    add icon-ghana.svg

--- main.js
+++ main.js
@@ -27,19 +27,18 @@ document.querySelector("#cards").innerHTML =
         createCard(
             '24/11' , 
             'quinta', 
-            createGame('brazil', '16:00', 'serbia')
+            createGame('switzerland', '07:00', 'cameroon' ) +
+            createGame('uruguay', '10:00', 'south-korea' ) +
+            createGame('portugal', '13:00', 'ghana') +            
+            createGame('brazil', '16:00', 'serbia')             
             ) +
 
         createCard(
             '28/11', 
             'segunda', 
+            createGame('cameroon', '07:00', 'serbia') +
+            createGame('south-korea', '10:00', 'ghana') +
             createGame('brazil', '13:00', 'switzerland') +
             createGame('portugal', '16:00', 'uruguay')
-            ) + 
-            
-        createCard(
-            '02/12', 
-            'sexta', 
-            createGame('brazil', '16:00', 'cameroon')
-            )
+            ) 
     
